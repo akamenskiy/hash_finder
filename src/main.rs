9044695cade33a108b98a5859dc82e5f977bf4f4
Aleksandr Kamenskii <kamenskogo@gmail.com>
2023-09-26 21:18:43 +0200

    took sequence change out of treads

--- src/main.rs
+++ src/main.rs
@@ -81,7 +81,7 @@ impl Worker {
 fn main() {
     let args: Arguments = argh::from_env();
     let start = Instant::now();
-    run(args);
+    test_run(args);
     let duration = start.elapsed();
 
     println!("Time elapsed in expensive_function() is: {:?}", duration);
@@ -91,25 +91,31 @@ fn run(args: Arguments) {
     let cores_amount = num_cpus::get();
     let pool = ThreadPool::new(cores_amount);
     let initial_state: Vec<HashPair> = Vec::new();
+    let initial_sequence: (usize, usize) = (0, 0);
     let result = Arc::new(Mutex::new(initial_state));
     let numbers_found = Arc::new(Mutex::new(0));
-    let current_number_sequence = Arc::new(Mutex::new(0));
+    let current_number_sequence = Arc::new(Mutex::new(initial_sequence));
     while *numbers_found.lock().unwrap() < args.f {
         let numbers_found = Arc::clone(&numbers_found);
         let result = Arc::clone(&result);
         let current_number_sequence = Arc::clone(&current_number_sequence);
-
+        let mut current_number_sequence = current_number_sequence.lock().unwrap();
+        current_number_sequence.0 = current_number_sequence.1 + 1;
+        current_number_sequence.1 = current_number_sequence.1 + 10000;
+        let (start, end) = *current_number_sequence;
         pool.execute(move || {
             let mut current_state = result.lock().unwrap();
-            let mut current_number_sequence = current_number_sequence.lock().unwrap();
-            *current_number_sequence += 1;
-            println!("{}", current_number_sequence);
-            let hash = digest(current_number_sequence.to_string());
-            let is_hash_valid = hash.chars().rev().take(args.n).all(|char| char == '0');
-            if is_hash_valid == true && *numbers_found.lock().unwrap() < args.f {
-                current_state.push(HashPair(*current_number_sequence, hash));
-                let mut numbers_found = numbers_found.lock().unwrap();
-                *numbers_found += 1;
+            for num in start..end {
+                if *numbers_found.lock().unwrap() == args.f {
+                    break;
+                }
+                let hash = digest(num.to_string());
+                let is_hash_valid = hash.chars().rev().take(args.n).all(|char| char == '0');
+                if is_hash_valid == true {
+                    current_state.push(HashPair(num, hash));
+                    let mut numbers_found = numbers_found.lock().unwrap();
+                    *numbers_found += 1;
+                }
             }
         });
     }
@@ -121,16 +127,15 @@ fn test_run(args: Arguments) {
     let initial_state: Vec<HashPair> = Vec::new();
     let mut result = initial_state;
     let mut numbers_found = 0;
-    let mut current_number_sequence = 1;
+    let mut current_number = 1;
     while numbers_found < args.f {
-        let hash = digest(current_number_sequence.to_string());
+        let hash = digest(current_number.to_string());
         let is_hash_valid = hash.chars().rev().take(args.n).all(|char| char == '0');
         if is_hash_valid == true {
-            println!("test {}: {}", current_number_sequence, hash);
-            result.push(HashPair(current_number_sequence, hash));
+            result.push(HashPair(current_number, hash));
             numbers_found += 1;
         }
-        current_number_sequence += 1;
+        current_number += 1;
     }
     result.iter().for_each(|x| println!("{}: {}", x.0, x.1));
 }
